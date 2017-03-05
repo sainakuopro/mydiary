8842a244936306d144159a0e6c187f660a3ddd10
sainakuopro <sainakuopro@gmail.com>
2017-03-05 17:51:23 +0300

    scrolling works!

--- js/js.js
+++ js/js.js
@@ -1,10 +1,16 @@
-var header = new Headhesive('.header');
-
+//Функция мигания кнопок "ВВерх-Вниз"
 setTimeout(function(){$('.down').fadeIn('slow')},1000); 
-
 setTimeout(function(){$('.up').fadeIn('slow')},1000);
 
-
-//comment
+//Функция скроллинга 
+$(document).ready(function() {
+$(".main").onepage_scroll({
+   sectionContainer: "section", 
+   easing: "ease",
+   animationTime: 1000,
+   pagination: true,
+   updateURL: false
+});
+});
 
 
